93ed2f72e5d098ee125634483c2b74d2022d3d55
Max Base <MaxBaseCode@Gmail.Com>
2020-10-28 17:05:49 +0330

    Added C° and adding Ident tab

--- send.php
+++ send.php
@@ -9,37 +9,37 @@ $_res=[
 $_res[0]=json_decode($_res[0][0], true);
 $_res[1]=json_decode($_res[1][0], true);
 $chs=[
-        '-***001294257647',
-        '-***001479103968',
+	'-***001294257647',
+	'-***001479103968',
 ];
 foreach($chs as $i=>$ch) {
-$res=$_res[$i];
-$chatID=[
-	'***', // Admin, User ID
-	'***', // Support Team, User ID
-	$ch,
-];
-$res='🌤  The weather today
+	$res=$_res[$i];
+	$chatID=[
+		'***', // Admin, User ID
+		'***', // Support Team, User ID
+		$ch,
+	];
+	$res='🌤  The weather today
 
 🔻 City: '.$res["location"]["name"].', '.$res["location"]["country"].'
 
-🔸 Temperature: '.$res["current"]["temperature"].' سانتی گراد
+🔸 Temperature: '.$res["current"]["temperature"].' C°
 🔸 Weather forecast: '.$res["current"]["weather_descriptions"][0].'
 🔸 Wind speed: '.$res["current"]["wind_speed"].'
-🔸 Sensible temperature:  '.$res["current"]["feelslike"].' سانتی گراد
+🔸 Sensible temperature:  '.$res["current"]["feelslike"].' C°
 🔸 Wind direction:  '.$res["current"]["wind_degree"].' '.$res["current"]["wind_dir"].'
 🔸 Time: '.$res["current"]["observation_time"].'
 
 * This message is automatically sent daily to the channel at the same time
 🔴 ';
-foreach($chatID as $chat) {
-	$ress=$res;
-	if($chat == '-***001479103968') {
-		$ress.='@aqaye_holland';
+	foreach($chatID as $chat) {
+		$ress=$res;
+		if($chat == '-***001479103968') {
+			$ress.='@aqaye_holland';
+		}
+		else if($chat == '-***001294257647') {
+			$ress.='@khanome_german';
+		}
+		sendMessage($chat, $ress);
 	}
-	else if($chat == '-***001294257647') {
-		$ress.='@khanome_german';
-	}
-	sendMessage($chat, $ress);
-}
 }
